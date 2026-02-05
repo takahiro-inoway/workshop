7554fdc7e190812f62d458aaf2abe0944920a055
takahiro-inoway <81753923+takahiro-inoway@users.noreply.github.com>
2026-02-05 14:07:42 +0900

    Add files via upload

--- birdgame_1/workshop_2.js
+++ birdgame_1/workshop_2.js
@@ -1,16 +1,3 @@
-// エンジンの更新ごと(毎フレーム)に実行される処理
-Events.on(engine, 'afterUpdate', () => {
-  // 発射中かつ鳥が発射点を越えたら、ゴムの制約を解除して自由に飛ばす
-  if (isFiring && tori.position.x > bane.x + 10) {
-    gattai.bodyB = null; // 接続を解除
-    gattai.render.visible = false;
-    isFiring = false;
-  }
-});
-
-// 世界(world)に全ての物体(地面、鳥、ゴム、マウス制約、箱の束)を追加
-Composite.add(world, [jimen, tori, gattai, mouseConstraint, ...hako]);
-
 // 描画とエンジンの実行を開始
 Render.run(render);
 const runner = Runner.create();
